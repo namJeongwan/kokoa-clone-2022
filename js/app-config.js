ad091b2bd5ee93f3b3b391d4e2d3fdc0f62452b0
namJeongwan <njw1107@naver.com>
2023-03-02 15:59:52 +0900

    .

---
+++ js/app-config.js
@@ -0,0 +1,6 @@
+export const chatGPTInfo = {
+    apiKey: "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",
+    // url: "https://api.openai.com/v1/completions",
+    url: "https://api.openai.com/v1/chat/completions",
+    model: "gpt-3.5-turbo"
+}
